f36360693cb724df7067ace117398f5acb0a78e2
Jamie Mill <jamiermill@gmail.com>
2013-03-02 15:58:29 +0000

    styles and script into external files

---
+++ main.js
@@ -0,0 +1,62 @@
+var width = 960,
+    height = 500;
+
+var projection = d3.geo.equirectangular()
+    .scale(150);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var graticule = d3.geo.graticule();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("path")
+    .datum(graticule.outline)
+    .attr("class", "background")
+    .attr("d", path);
+
+svg.append("g")
+    .attr("class", "graticule")
+  .selectAll("path")
+    .data(graticule.lines)
+  .enter().append("path")
+    .attr("d", path);
+
+svg.append("path")
+    .datum(graticule.outline)
+    .attr("class", "foreground")
+    .attr("d", path);
+
+d3.json("/world-110m.json", function(error, world) {
+  svg.insert("path", ".graticule")
+      .datum(topojson.object(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a.id !== b.id; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+
+
+  d3.csv('/country_latlon.csv', function(error, countryLocations) {
+
+    var countriesToPlot = ['US', 'GB', 'JP', 'BR'];
+
+
+    _.each(countriesToPlot, function(countryToPlot) {
+
+      var country = _.find(countryLocations, function(country) { return country['iso 3166 country'] === countryToPlot; })
+      var countryLocation = projection([country.longitude, country.latitude]);
+      svg.append("circle")
+        .attr("cx", countryLocation[0])
+        .attr("cy", countryLocation[1])
+        .attr("r", 10)
+        .attr("class", "country-point");
+    });
+  });
+
+});
